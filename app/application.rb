b6f5fdd5cfb651625dcf70a83b6fdad63905fc8d
Gonzalez32 <3232rose@gmail.com>
2020-01-27 01:11:31 -0600

    Done.

--- app/application.rb
+++ app/application.rb
@@ -13,6 +13,17 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.length == 0
+        resp.write "Your cart is empty."
+      else @@cart.each do |item|
+        resp.write "#{item}\n"
+      end 
+    end
+
+    elsif req.path.match(/add/)
+      item_to_add = req.params["item"]
+      resp.write handle_add(item_to_add)
     else
       resp.write "Path Not Found"
     end
@@ -20,9 +31,18 @@ class Application
     resp.finish
   end
 
+  def handle_add(item_to_add)
+    if @@items.include?(item_to_add)
+      @@cart << item_to_add
+      return "added #{item_to_add}\n"
+    else
+      return "#{item_to_add} Is not in our inventory. We don't have that item."
+    end
+  end
+
   def handle_search(search_term)
     if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
+      return "#{item_to_add} is one of our items"
     else
       return "Couldn't find #{search_term}"
     end
